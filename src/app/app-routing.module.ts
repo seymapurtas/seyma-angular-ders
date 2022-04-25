23315fadb2088a3e3efd2a80c1bae7a2d6e4c440
Şeyma Purtaş <seymapurtas@Seyma-MacBook-Air.local>
2022-04-25 18:23:17 +0300

    Add Pages Sturcure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'homepage',
+    loadChildren: () =>
+      import('./pages/homepage/homepage.module').then((m) => m.HomepageModule),
+  },
+  { path: 'users', loadChildren: () => import('./pages/users/users.module').then(m => m.UsersModule) },
+  { path: 'cars', loadChildren: () => import('./pages/cars/cars.module').then(m => m.CarsModule) },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'seyma-angular-ders';
+
+  age = 23;
+
+  ngOnInit(): void {
+    console.log('seyma');
+    if (this.age > 30) {
+      console.log("seyma 23 yaşından büyük!");
+    } 
+    
+    else {
+      console.log("seyma 23 yaşından küçük!");
+    }
+
+  }
 }
